c740651c34e31414e9df41d8f5e9170babbbc381
Debesh Mondal <debeshprofessional05@gmail.com>
2024-06-12 11:48:50 +0530

    chore: Add nested loops and for loops examples

---
+++ for_loops.py
@@ -0,0 +1,23 @@
+# for item in 'python':
+#     print(item)
+
+# for item in ['python', 'Java', 'Ruby']:
+#     print(item)
+
+# for item in ['1', '4', '74']:
+#     print(item)
+
+# for item in range (10):
+#     print(item)
+
+# for item in range (1,11):
+#     print(item)
+
+prices = [10,20,30]
+total = 0
+
+for price in prices:
+    total += price
+print(f"The total price is: {total}")
+    
+
